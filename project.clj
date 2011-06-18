18e8b314a03984aa3ed676012d9af44ad8b018a3
Mike Ivanov <mike@mikeivanov.com>
2011-06-17 19:29:48 -0700

    Added incanter dependency

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject thinkstats "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "My Think Stats book worksheet"
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [incanter "1.2.3"]]
   :extra-classpath-dirs ["resources"])
