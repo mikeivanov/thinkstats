37a5926c01b9c55d22fda404cb565556836aa052
Mike Ivanov <mike@mikeivanov.com>
2011-06-28 20:16:15 -0700

    Added a histogram chart example

---
+++ src/thinkstats/exercises/ex_histogram.clj
@@ -0,0 +1,30 @@
+(ns thinkstats.exercises.ex-histogram
+  (:use clojure.contrib.generic.math-functions
+        [thinkstats.survey :only (dataset firstborn? lengths)])
+  (:require [incanter.core   :as ic]
+            [incanter.charts :as charts])
+  (:import  (org.jfree.chart.renderer.xy ClusteredXYBarRenderer)))
+
+(defn alive-histogram []
+  (let [data (lengths (dataset :alive))
+        hist (charts/histogram data)]
+    (ic/view hist)))
+
+(defn in-range? [rec]
+  (let [x (:prglength rec)]
+    (and (< 25 x) (<= x 45))))
+
+(defn hist-2 []
+  (let [records    (filter in-range? (dataset :alive))
+        firstborns (group-by firstborn? records)
+        fb-len     (lengths (firstborns true))
+        ob-len     (lengths (firstborns false))
+        hist       (doto (charts/histogram fb-len :legend true)
+                     (charts/add-histogram ob-len))]
+    (doto (.getPlot hist)
+      (.setRenderer (ClusteredXYBarRenderer.)))
+    (ic/view hist)))
+
+
+
+
