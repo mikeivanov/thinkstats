3367b7d767e4e69db8b7503e35c72a1ec499f6bb
Mike Ivanov <mike@mikeivanov.com>
2011-07-15 00:26:12 -0700

    Aggregated exercises showcase

---
+++ src/thinkstats/exercises.clj
@@ -0,0 +1,18 @@
+(ns thinkstats.exercises
+  (:require [thinkstats.exercises ex-1-3 ex-2-1]))
+
+(defmacro ex [id]
+  (let [module (str "thinkstats.exercises." (str id))
+        rqsym  (symbol module)
+        fnsym  (symbol (str module "/-main"))]
+    `(do (println (str "--------> " '~id))
+         (require '~rqsym)
+         (~fnsym))))
+
+(defn -main []
+  (ex ex-1-3)
+  (ex ex-2-1)
+  (ex ex-2-3)
+  (ex ex-2-4)
+  (ex ex-2-5))
+
