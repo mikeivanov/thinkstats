26d78710d9f579adbf5ef802cce57a083a7c9e47
Mike Ivanov <mike@mikeivanov.com>
2012-03-24 16:00:25 -0400

    Upgraded to Clojure 1.3 and Incanter 1.3

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject thinkstats "1.0.0-SNAPSHOT"
   :description "My Think Stats book worksheet"
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.clojure/clojure-contrib "1.2.0"]
-                 [incanter "1.2.3"]]
-  :extra-classpath-dirs ["resources"])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [incanter "1.3.0-SNAPSHOT" :exclusions [swank-clojure]]]
+  :extra-classpath-dirs ["resources"]
+  :plugins [[lein-swank "1.4.3"]])
