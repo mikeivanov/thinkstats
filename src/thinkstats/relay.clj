2311ea364a8b3acdaa56a30264d2842ecf6d4f4f
Mike Ivanov <me@mikeivanov.com>
2012-12-28 04:02:28 -0500

    Rewrite the relay data scraper

--- src/thinkstats/relay.clj
+++ src/thinkstats/relay.clj
@@ -9,31 +9,54 @@
 ;;     1   1/362  M2039   30:43   30:42   4:57 Brian Harvey           ...
 ;;     2   2/362  M2039   31:36   31:36   5:06 Mark Miller            ...
 
-(def record-rx
-  #"(?x)^
-    \s+\d+\s+           # Place
-    (?:\d+/\d+\s+)?     # Div/Tot
-    (?:\w\d{4}\s+)?     # Div
-    (?:\d\d:\d\d\s+){2} # Guntime and Nettime
-    (\d+):(\d\d)\s+     # Pace <= this is what we are after
-    ")
-
-(defn- parse-record [line]
-  (if-let [[_ mstr sstr] (re-find record-rx line)]
-    (let [[min sec] (map #(Integer/parseInt %) [mstr sstr])
-          pace      ($= sec + min * 60.0)]
-      (double pace))))
+(defn- parse-time [time]
+  (if-let [min-sec  (re-find #"(\d+):(\d+)" time)]
+    (let [[min sec] (map parse-int (drop 1 min-sec))
+          seconds   ($= sec + min * 60.0)]
+      (double seconds))))
 
 (defn- pace-to-speed [pace]
   ($= 60.0 * 60.0 / pace))
 
-(def *run-results-url*
-  "http://coolrunning.com/results/10/ma/Apr25_27thAn_set1.shtml")
+(defn- re-group-keys [rx]
+  (map (comp keyword second)
+       (re-seq #"\(\?<([^>]+)>" (str rx))))
 
-(defn speeds []
-  (->> *run-results-url*
-       get-input-reader
-       read-lines
-       (keep parse-record)
-       (map pace-to-speed)))
+(defn- scrape
+  ([rx lines]
+     (scrape rx (re-group-keys rx) lines))
+  ([rx columns lines]
+     (dataset columns
+              (->> lines
+                   (map (partial re-find rx))
+                   (filter (complement nil?))
+                   (map rest)))))
+
+(def race-rx #"(?x)^\s+
+               (\d+)\s+             # (?<place>)
+               (?:(\d+)/(\d+))?\s+  # (?<divpos>) (?<divtot>)
+               (?:([MF]\d{4}))?\s+  # (?<division>)
+               (\d+:\d+)\s+         # (?<guntime>)
+               (\d+:\d+)\s+         # (?<nettime>)
+               (\d+:\d+)\s+         # (?<pace>)
+               (.*?)\s+             # (?<name>)
+               (?:(\d+))?\s+        # (?<age>)
+               (?:(M|F))?\s+        # (?<sex>)
+               (\d+)\s+             # (?<racenum>)
+               (\w.+?)\s([A-Z]{2})  # (?<city>) (?<state>)
+               \s*$")
+
+(def ^:dynamic *run-results-url*
+  ;;"http://coolrunning.com/results/10/ma/Apr25_27thAn_set1.shtml"
+  "/Users/mike/projects/thinkstats/book/workspace.thinkstats/ThinkStats/Apr25_27thAn_set1.shtml"
+  )
+
+(defn race-dataset [& [url]]
+  (let [url (or url *run-results-url*)
+        ds  (->> url
+                 (read-lines)
+                 (scrape race-rx))
+        speeds ($map (comp pace-to-speed parse-time) :pace ds)]
+    (conj-cols ds
+               (dataset [:speed] speeds))))
 
