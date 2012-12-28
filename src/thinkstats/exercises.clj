01805a8ebf67275e0c219b84429f93fc88d0400a
Mike Ivanov <me@mikeivanov.com>
2012-12-28 04:03:09 -0500

    Get rid of the `ex` macro, rewrite it as a function

--- src/thinkstats/exercises.clj
+++ src/thinkstats/exercises.clj
@@ -1,29 +1,32 @@
-(ns thinkstats.exercises)
+(ns thinkstats.exercises
+  (use [clojure.string :only [replace]]))
 
-(defmacro ex [id]
-  (let [module (str "thinkstats.exercises." (str id))
-        rqsym  (symbol module)
-        fnsym  (symbol (str module "/-main"))]
+(defn call [name & args]
+    (when-let [fun (ns-resolve *ns* (symbol name))]
+      (apply fun args)))
+
+(defn ex [id]
+  (let [path (str "src/thinkstats/exercises/" (replace (str id) #"-" "_") ".clj")
+        name (str "thinkstats.exercises." id "/-main")]
     (do
-      (require rqsym)
-      `(do (println (str "--------> " '~id))
-           (~fnsym)))))
+      (load-file path)
+      (println (str "--------> " id))
+      (call name))))
 
 (defn -main []
-  (ex ex-1-3)
-  (ex ex-2-1)
-  (ex ex-2-3)
-  (ex ex-histogram)
-  (ex ex-2-4)
-  (ex ex-2-5)
-  (ex ex-2-6)
-  (ex ex-2-8)
-  (ex ex-2-7)
-  (ex ex-3-1)
-  (ex ex-3-2)
-  (ex ex-3-3)
-  (ex ex-3-4)
-  (ex ex-3-5)
-  (ex ex-3-6)
-  (ex ex-3-7))
-
+  (ex 'ex-1-3)
+  (ex 'ex-2-1)
+  (ex 'ex-2-3)
+  (ex 'ex-histogram)
+  (ex 'ex-2-4)
+  (ex 'ex-2-5)
+  (ex 'ex-2-6)
+  (ex 'ex-2-7)
+  (ex 'ex-2-8)
+  (ex 'ex-3-1)
+  (ex 'ex-3-2)
+  (ex 'ex-3-3)
+  (ex 'ex-3-4)
+  (ex 'ex-3-5)
+  (ex 'ex-3-6)
+  (ex 'ex-3-7))
