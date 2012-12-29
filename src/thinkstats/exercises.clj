06899ca15791aafd078e21d08387fc1a8d814b2c
Mike Ivanov <me@mikeivanov.com>
2012-12-29 02:06:47 -0500

    Simplify the exercise runner

--- src/thinkstats/exercises.clj
+++ src/thinkstats/exercises.clj
@@ -1,35 +1,21 @@
 (ns thinkstats.exercises
-  (use [clojure.string :only [replace]]))
+  (require [clojure.string :as string]))
 
 (defn call [name & args]
     (when-let [fun (ns-resolve *ns* (symbol name))]
       (apply fun args)))
 
-(defn ex [id]
-  (let [path (str "src/thinkstats/exercises/" (replace (str id) #"-" "_") ".clj")
-        name (str "thinkstats.exercises." id "/-main")]
+(defn exercise [id]
+  (let [path (str "src/thinkstats/exercises/ex_" (string/replace id #"-" "_") ".clj")
+        name (str "thinkstats.exercises.ex-" id "/-main")]
     (do
       (load-file path)
       (println (str "--------> " id))
       (call name))))
 
+(def exercises ["1-3" "2-1" "2-3" "histogram" "2-4" "2-5" "2-6" "2-7" "2-8"
+                "3-1" "3-2" "3-3" "3-4" "3-5" "3-6" "3-7" "3-8" "3-9" "3-10"])
+
 (defn -main []
-  (ex 'ex-1-3)
-  (ex 'ex-2-1)
-  (ex 'ex-2-3)
-  (ex 'ex-histogram)
-  (ex 'ex-2-4)
-  (ex 'ex-2-5)
-  (ex 'ex-2-6)
-  (ex 'ex-2-7)
-  (ex 'ex-2-8)
-  (ex 'ex-3-1)
-  (ex 'ex-3-2)
-  (ex 'ex-3-3)
-  (ex 'ex-3-4)
-  (ex 'ex-3-5)
-  (ex 'ex-3-6)
-  (ex 'ex-3-7)
-  (ex 'ex-3-8)
-  (ex 'ex-3-9)
-  (ex 'ex-3-10))
+  (doseq [id exercises]
+    (exercise id)))
